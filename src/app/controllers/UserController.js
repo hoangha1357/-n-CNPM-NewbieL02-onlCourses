7edd32b1f4a1d51caff6d2a7cc6c4342976beeb0
lenghia2797 <93039827+lenghia2797@users.noreply.github.com>
2021-11-26 20:49:36 +0700

    update register course

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -150,29 +150,14 @@ class UserController {
     
     // [GET] /user/registeredCourse
     viewRegisteredCourse(req, res, next) {
-        User.findOne({email: req.session.email.username})
-            .then(async function(user) {
-                user = user.toObject();
-                var numCourse = user.registeredCourseIds.length;
-                console.log('Courses : ', numCourse);
-                var courses = [];
-                for (var i = 0; i < numCourse; i++) {
-                    await Course.findOne({_id: user.registeredCourseIds[i]}) 
-                        .then(course => {   courses.push({  _id: course._id, 
-                                                name: course.name,
-                                                description: course.description,
-                                                author: course.author,
-                                                slug: course.slug,
-                                            })
-                        })
-                        .catch((error) => res.json({message: error.message}));
-                }
-                res.render('user/registeredCourse',{
-                    courses: courses,
-                    user: req.user, 
+        Course.find({ _id: { $in : req.user.registeredCourseIds} })
+            .then((registeredCourses) => {
+                res.render('User/registeredCourse', {
+                    courses: mutiMongoosetoObject(registeredCourses)
                 })
             })
-            .catch((error) => res.json({message: error.message}));
+            .catch(next);
+        
     }
 
     // [GET] /user/resetpassword/:id/:token
